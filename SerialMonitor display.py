374c78be8f0f54c16fd577710ce761ab58d8f611
Maks <64848044+maks7d@users.noreply.github.com>
2022-12-27 08:09:12 +0000

    stable save
    save for changing linux

--- SerialMonitor display.py
+++ SerialMonitor display.py
@@ -2,6 +2,7 @@ from tkinter import *
 import serial.tools.list_ports
 import functools
 
+
 ports = serial.tools.list_ports.comports()
 serialObj = serial.Serial()
 
@@ -16,6 +17,7 @@ menu.add_cascade(label="Ports", menu=Ports)
 fenetre.config(menu=menu)
 
 chronos_max = []
+chronos_Hugo=[]
 
 def initComPort(index):
     currentPort = str(ports[index])
@@ -40,25 +42,52 @@ dataCanvas.config(yscrollcommand = vsb.set)
 dataFrame = Frame(dataCanvas, bg="white")
 dataCanvas.create_window((10,0),window=dataFrame,anchor='nw')
 
-
 def checkSerialPort():
+    dernier_chrono=[]
+    dernier_chrono.clear()
+    debut_chrono=5
     if serialObj.isOpen() and serialObj.in_waiting:
-        recentPacket = serialObj.readline()
-        recentPacketString = recentPacket.decode('utf').rstrip('\n')
-        Label(dataFrame, text=recentPacketString, bg = "white").pack()
-        return recentPacketString
+        #traitement du message 
+        recentPacket = str(serialObj.readline())
+        recentPacket = recentPacket[: -5 or None]
+        recentPacket += "}"
+        #Label(dataFrame, text=recentPacket, bg = "white").pack()
+        
+        if recentPacket[2] == "P": #si message de type puce numero...
+            if recentPacket[3] == '1': #si puce numero 1 (hugo)
+                Label(dataFrame, text="Hugo : ").pack() 
+                if(recentPacket[debut_chrono-1] == 'C'): #si caractere 'c' : debut du dernier chrono
+                    i=debut_chrono
+                    while(recentPacket[i] != "}"): #parcourt le tableau depuis le chrono
+                        dernier_chrono.append(recentPacket[i])
+                        i+=1
+                    #conversion du tab dernier_chrono en int 
+                    s = [str(integer) for integer in dernier_chrono]
+                    dernier_chrono = "".join(s)
+                    #ajout du dernier chrono dans le tab chronos_hugo
+                    chronos_Hugo.append(int(dernier_chrono))
+                    Label(dataFrame, text=chronos_Hugo[len(chronos_Hugo)-1]).pack()
 
-def decode_chronos():
-    #gestion chrono recu
-    if(dernier_packet_reçu == "chronos"):
-        print("chronos")
+            elif recentPacket[3] == '2': #si puce numero 2 (maxime)
+                Label(dataFrame, text="Maxime").pack()
+                if(recentPacket[debut_chrono-1] == 'C'):
+                    i=debut_chrono
+                    while(recentPacket[i] != "}"):
+                        dernier_chrono.append(recentPacket[i])
+                        i+=1
+                    #conversion du tab dernier_chrono en int
+                    s = [str(integer) for integer in dernier_chrono]
+                    dernier_chrono = "".join(s)
+                    chronos_max.append(int(dernier_chrono))
+                    Label(dataFrame, text=chronos_max[len(chronos_max)-1]).pack()
 
-
+#def decode_msg(packet, chronos):
+    
 while True:
     fenetre.update()
     checkSerialPort()
     dataCanvas.config(scrollregion=dataCanvas.bbox("all"))
-    dernier_packet_reçu = str(checkSerialPort())
-    decode_chronos()
+    
+    
     
 
